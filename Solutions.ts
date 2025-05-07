a7a5a9419472f145528bd4fab7bbf4bfd845c217
Rika4698 <rikasharmin181@gmail.com>
2025-05-08 01:25:00 +0600

    All Problem Solution

---
+++ Solutions.ts
@@ -0,0 +1,114 @@
+
+function formatString(input:string, toUpper:boolean = true):string{
+    const formate = (toUpper ? input.toUpperCase() : input.toLowerCase());
+    return formate;
+
+}
+
+
+
+ function filterByRating(items:{title:string; rating:number}[]) : {title:string; rating:number}[]{
+    const itemFilter = items.filter(product => product.rating >= 4);
+    return itemFilter;
+ }
+ 
+
+
+
+
+
+function concatenateArrays<T>(...arrays:T[][]):T[]{
+    const allArrays = ([]as T[]).concat(...arrays);
+    return allArrays;
+
+}
+
+
+
+
+
+
+class Vehicle {
+    private make: string;
+    private year: number;
+    constructor(make:string, year:number){
+        this.make = make;
+        this.year = year;
+    }
+    getInfo():string{
+        return `"Make: ${this.make}, Year: ${this.year}"`
+    }
+}
+class Car extends Vehicle{
+    private model: string;
+    constructor(make:string,year:number,model:string){
+        super(make, year);
+        this.model = model;
+    }
+    getModel(): string{
+        return `"Model: ${this.model}"`
+    }
+}
+
+
+
+
+function processValue(value: string|number):number{
+     
+    const process = (typeof value === 'string'? value.length: value * 2);
+    return process;
+}
+
+
+
+
+
+interface Product{
+    name: string;
+    price: number;
+}
+function getMostExpensiveProduct(products: Product[]): Product | null{
+    if(products.length === 0) return null;
+    const expensiveProduct = products.reduce((highest , current) => current.price > highest.price ? current:highest );
+    return expensiveProduct;
+}
+
+
+
+
+
+enum Day {
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  }
+  function getDayType(day: Day): string{
+    const weekDay = (day === Day.Saturday|| day === Day.Sunday?"Weekend":"Weekday");
+    return weekDay;
+  }
+
+
+
+
+
+
+  async function squareAsync(n: number): Promise<number>{
+
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            n<0? reject(new Error("Negative number not allowed")):resolve(n*n);
+        },1000 );
+    });
+  }
+
+
+
+
+
+
+
+
